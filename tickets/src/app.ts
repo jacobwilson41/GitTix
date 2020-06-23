5509275d9b7164ccd62b70cd7cf158b889c8de75
Jacob Wilson <jacobcharles96@gmail.com>
2020-06-22 21:09:28 -0700

    Tests for updating existing tickets
    The last test for updating a ticket is expecting a 200 OK but responding with a
    401 Unauthorized.  I tested it manually with Postman and its working correctly so I'm going to leave it for now

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import { createTicketRouter } from './routes/new';
 import { errorHandler, NotFoundError, currentUser } from '@jwtickets/common';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,6 +22,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
