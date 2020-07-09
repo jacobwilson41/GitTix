7c81541dc8fba05a4431c951bc9f8832c80d080d
Jacob Wilson <jacobcharles96@gmail.com>
2020-07-09 19:05:15 -0400

    Expiration:complete listener emits the order:cancelled event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,7 +9,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-
+    console.log(`Waiting ${delay} milliseconds to process the job`);
     await expirationQueue.add(
       {
         orderId: data.id
