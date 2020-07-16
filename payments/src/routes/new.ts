1598ed515b32ddce85474be3dde5b8c0f0e62b55
Jacob Wilson <jacobcharles96@gmail.com>
2020-07-16 17:05:55 -0400

    functionality for creating a charge with stripe and passing tests

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@jwtickets/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -20,7 +21,13 @@ router.post('/api/payments', requireAuth, [body('token').not().isEmpty(), body('
     throw new BadRequestError('Cannot pay for a cancelled order');
   }
 
-  res.send({ success: true });
+  await stripe.charges.create({
+    currency: 'usd',
+    amount: order.price * 100,
+    source: token
+  });
+
+  res.status(201).send({ success: true });
 });
 
 export { router as createChargeRouter };
