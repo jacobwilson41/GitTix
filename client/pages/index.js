4075c6a44da15fea8409807f61149e19137d0642
Jacob Wilson <jacobcharles96@gmail.com>
2020-07-17 12:24:16 -0400

    new ticket form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,9 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
   return <h1>You are {!currentUser && 'not'} signed in.</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
